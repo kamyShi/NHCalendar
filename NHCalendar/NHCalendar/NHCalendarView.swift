b79b8b63d0ad2dd2e28f662ce3fc8d2a05bb3f42
牛辉 <niuhui@niuhuideMacBook-Pro.local>
2016-02-15 22:49:09 +0800

    增加备注

---
+++ NHCalendar/NHCalendar/NHCalendarView.swift
@@ -0,0 +1,21 @@
+//
+//  NHCalendarView.swift
+//  NHCalendar
+//
+//  Created by 牛辉 on 16/2/15.
+//  Copyright © 2016年 Niu. All rights reserved.
+//
+
+import UIKit
+
+class NHCalendarView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- NHCalendar/NHCalendar/NHDate_Categroy.swift
+++ NHCalendar/NHCalendar/NHDate_Categroy.swift
@@ -11,15 +11,17 @@ extension NSDate {
     /**
      获取这个月有多少天
      */
-    func getMonthHowManyDay() ->Int {
+    func getMonthHowManyDay() -> Int {
+        //我们大致可以理解为：某个时间点所在的“小单元”，在“大单元”中的数量
         return NSCalendar.currentCalendar().rangeOfUnit(.Day, inUnit:.Month, forDate: self).length
     }
     /**
      *  获取这个月第一天是星期几
      */
-    func getMontFirstWeekDay() ->Int {
+    func getMontFirstWeekDay() -> Int {
         //1.Sun. 2.Mon. 3.Thes. 4.Wed. 5.Thur. 6.Fri. 7.Sat.
         let calendar = NSCalendar.currentCalendar()
+        //这里注意 swift要用[,]这样方式写
         let com = calendar.components([.Year,.Month,.Day], fromDate: self)
         //设置成第一天
         com.day = 1
